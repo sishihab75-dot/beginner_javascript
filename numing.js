6d6ecc5e1c406d4fdb2b99aab9b2e9255e2db525
sishihab75-dot <sishihab75@gmail.com>
2026-07-28 19:02:03 +0600

    add float number , +&- infinity

---
+++ numing.js
@@ -0,0 +1,31 @@
+var orangeprice = 20 ;
+var chokolate = 0.5 ;
+var kola = parseInt("50.5");
+var kola1 = parseFloat("50.5");
+
+console.log(orangeprice);
+console.log(chokolate);
+console.log(typeof chokolate);
+console.log(kola);
+console.log(kola1);
+console.log(orangeprice + chokolate + kola);
+
+// float number foe requard 
+
+var first = 0.1 ;
+var second = 0.2 ;
+var total =first + second;
+console.log(total);      
+
+// problem fixed 
+var first = 0.1 ;
+var second = 0.2 ;
+var total =first + second;
+console.log(total.toFixed(5));         
+
+// + & - infinity 
+
+const num = 50/0;
+console.log(num);
+const num1 = -50/0;
+console.log(num1);
