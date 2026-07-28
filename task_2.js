3f0942a80c22c477733b9756e892bcc0b3f995f9
sishihab75-dot <sishihab75@gmail.com>
2026-07-28 22:11:10 +0600

    task_2 complite

---
+++ task_2.js
@@ -0,0 +1,8 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var total = (mathematics + biology + chemistry + physics + bangla);
+var average_marks = (total/5);
+console.log(average_marks.toFixed(2));
