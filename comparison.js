e6b3d6382571024c9b54b1ae168b71e717d7a389
sishihab75-dot <sishihab75@gmail.com>
2026-08-02 17:20:58 +0600

    add if else

--- comparison.js
+++ comparison.js
@@ -4,3 +4,28 @@ console.log(5>=10);
 console.log(5<=10);
 console.log(5==10);
 console.log(5!=10);
+
+
+// comparison on variable 
+
+var bal = 10;
+var chal = 15;
+console.log(bal > chal);
+
+//  if & else 
+
+var number = 5;
+if(5>3){
+    console.log("hello");
+}
+if(5>6){
+    console.log("hello");
+}
+
+if(number > 8)
+{
+    console.log("drink water :");
+}
+else{
+    console.log("dont drink water :");
+}
