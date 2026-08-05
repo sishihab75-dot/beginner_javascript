1c96b7241b3e4b2fab845ce33b28f084fdc4a134
sishihab75-dot <sishihab75@gmail.com>
2026-08-05 16:03:17 +0600

    add array

---
+++ array.js
@@ -0,0 +1,15 @@
+const number = [10,11,12,13,14,15,16,17,18,19,20,]
+console.log("Total array elimant :" , number.length);
+console.log("This index number eliment valu is :",number[5]);
+
+// for(const num of number){
+//     console.log(num);
+// }
+
+// for (const [index, num] of number.entries()) {
+//     console.log("Index:", index, "Value:", num);
+// }
+
+for(let i=0; i<number.length;i++){
+    console.log("index :",i,"valu :",number[i]);
+}
