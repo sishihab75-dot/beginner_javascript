434c894169e50121838bbc28303337dec1487d9d
sishihab75-dot <sishihab75@gmail.com>
2026-08-12 17:08:33 +0600

    string compeyare

---
+++ sting_compeyar.js
@@ -0,0 +1,15 @@
+const book = "bangla";
+const book1 = "Bangla";
+if(book == book1){
+    console.log("khela hobe :");
+}
+else{
+    console.log("kothin kup :");
+}
+
+if(book.toUpperCase == book1.toUpperCase){
+    console.log("khela hobe :");
+}
+else{
+    console.log("kothin kup :");
+}
