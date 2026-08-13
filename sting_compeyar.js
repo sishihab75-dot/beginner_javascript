69f44259190badf8b565da92f02694155ca4d347
sishihab75-dot <sishihab75@gmail.com>
2026-08-13 22:08:17 +0600

    object

--- sting_compeyar.js
+++ sting_compeyar.js
@@ -13,3 +13,36 @@ if(book.toUpperCase == book1.toUpperCase){
 else{
     console.log("kothin kup :");
 }
+
+// slice 
+
+const address = "voashrwala";
+const part = address.slice(3,7);
+console.log(part);
+
+// split 
+
+console.log(address.split("a"));
+const array= [1,2, 3, 4, 5, 5,6,3];
+console.log(array.join("_"));
+
+// revarse 
+
+const revarse = "i learning fahh"
+let fah = ""
+for(const letter of revarse){
+    // console.log(letter);
+    fah = letter + revarse;
+}
+console.log(fah);
+
+
+//  object 
+
+const bottle= {
+    brand: "apple",
+    price: 50,
+    color: "red",
+    isclean: false
+}
+console.log(bottle);
